87179bae50331cbd5a805392603e1943dc690412
unknown <stefano.tonazzi@Tonazzi_S.ads.it>
2014-01-13 16:38:25 +0100

    DB conn decoupled

--- php/functions.php
+++ php/functions.php
@@ -1,4 +1,10 @@
 <?PHP
+require 'conn.php'; // DB connection
+/*
+ *$dbconn = pg_connect ("host=10.148.5.226 port=5432 dbname=notarilia user=postgres password=postgres") or die ('no db');
+*/
+
+
 function get_items_list($dbconn,$sql)
 {
    
@@ -16,7 +22,6 @@ function get_items_list($dbconn,$sql)
  *
  * @return string Indented version of the original JSON string.
  */
-$dbconn = pg_connect ("host=localhost port=5432 dbname=notarilia user=postgres password=postgres") or die ('no db');
 
 function indent($json) {
 
